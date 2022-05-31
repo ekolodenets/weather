0402a36d3fdac0cc36ed4b3168ca6da2af013bcb
ekolodenets <66313736+ekolodenets@users.noreply.github.com>
2022-05-31 13:11:50 +0300

    Initial commit

---
+++ coordinates.py
@@ -0,0 +1,8 @@
+from typing import NamedTuple
+
+class Coordinates(NamedTuple):
+    latitude: float
+    longitude: float
+
+def get_coordinates() -> Coordinates:
+    return Coordinates(latitude=10, longitude=20)
---
+++ weather_api_service.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from typing import NamedTuple
+
+from .coordinates import Coordinates
+
+Celsius = int
+
+class Weather(NamedTuple):
+    pass
+
+def get_weather(coordinates: Coordinates):
+    pass
+
