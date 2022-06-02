a4a433b4be4c319c3401489709d586791521c334
Evgeny Kolodenets <66313736+ekolodenets@users.noreply.github.com>
2022-06-02 23:04:40 +0300

    Update weather_formatter.py

--- weather_formatter.py
+++ weather_formatter.py
@@ -2,7 +2,7 @@ from weather_api_service import Weather
 
 def format_weather(weather: Weather) -> str:
     """Formats weather data in string"""
-    return (f'{weather.city}, температура {weather.temperature}C, {weather.weather_type.value}\n'
+    return (f'{weather.city}, температура {weather.temperature}°C, {weather.weather_type.value}\n'
             f'Восход: {weather.sunrise.strftime("%H:%M")}\n'
             f'Закат: {weather.sunset.strftime("%H:%M")}\n')
 
